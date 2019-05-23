70a1624e23fba976f15cd96075bf0cda442fcc20
aavondet <arnaud.avondet@plaid.co.jp>
2019-05-23 14:33:44 +0900

    lunchRouter Supports ID Requests
    -Can get, update, delete specific lunches

--- app/routers/lunchRouter.js
+++ app/routers/lunchRouter.js
@@ -38,4 +38,36 @@ lunchRouter.route('/')
     .catch((err) => next(err));
 });
 
+lunchRouter.route('/:id')
+.get((req, res, next) => {
+    Lunch.findById(req.params.id)
+    .then((lunch) => {
+        console.log('Gathered lunch with id: ' + req.params.id);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(lunch);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    Lunch.findByIdAndUpdate(req.params.id, req.body, { new : true})
+    .then((lunch) => {
+        console.log('Updated lunch ' + req.params.id + ' to ' + lunch);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(lunch);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Lunch.findByIdAndDelete(req.params.id)
+    .then((lunch) => {
+        console.log('Lunch ' + req.params.id + ' deleted...');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json'); 
+        res.json(lunch);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
 module.exports = lunchRouter;
